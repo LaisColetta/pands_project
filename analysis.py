5887ac819cb68c003a32152631170653521fee36
LaisColetta <laiscoletta@gmail.com>
2022-04-07 21:28:48 +0100

    project

--- analysis.py
+++ analysis.py
@@ -1,1 +1,14 @@
+# Program that analyses the Iris date set
 # Author; Lais Coletta
+
+# I am using pandas 
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+#download the data
+df = pd.read_csv('iris_csv.csv')
+#print out using the head function
+df.head()
+
